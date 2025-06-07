2920863b25f6b7fff5420630ad2beaad146b926a
Entairon <breakfaders1@gmail.com>
2025-06-07 13:07:00 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000  ni caso a los comentarios xd 
+  // ALWAYS serve the app on port 5000  holis 
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
