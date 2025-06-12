233571a0289288eb61f63c3398be3ff36f26f1e1
Entairon <breakfaders1@gmail.com>
2025-06-12 06:14:01 +0200

    Update HabboAvatar.tsx

--- client/src/components/HabboAvatar.tsx
+++ client/src/components/HabboAvatar.tsx
@@ -151,7 +151,7 @@ export function HabboAvatar({
             )}
             {habboUser && (
               <p className="text-xs text-white/50">
-                {selectedServer === 'origins' ? 'Habbo Origins (.ES)' : 'Habbo (.ES)'}
+                {selectedServer === 'origins' ? 'Habbo Origins' : 'Habbo ES'}
               </p>
             )}
           </div>
